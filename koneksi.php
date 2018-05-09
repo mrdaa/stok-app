50adc52dfd534f9e2382a47329fa49972605cd6e
unknown <d.aji.apriyadi@gmail.com>
2018-05-09 23:28:30 +0700

    oke

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class Koneksi {
     private $server = "localhost";
-    private $username = "root"; // root
-    private $password = ""; // ""
-    private $db = "phb_kelas_c";
+    private $username = "id4905153_mrdaa"; // root
+    private $password = "april@#1997"; // ""
+    private $db = "id4905153_stok";
     function getKoneksi() {
         return new mysqli($this->server, $this->username, 
         $this->password, $this->db);
